3a06584f07d0f93ce8308cf0831ce73e734e6776
Jalmido <hjallebgren@gmail.com>
2025-11-11 15:58:00 +0100

    Tillagt pistol som typ funkar och bättre fungerande player

---
+++ Scenes/gun.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+var mouseposition
+var bullet_velocity = 3000
+var is_shooting = false
+var bullet_direction = Vector2.ZERO
+
+
+
+@onready var bullet: Sprite2D = $Bullet
+
+func _process(delta: float) -> void:
+	mouseposition = get_global_mouse_position()
+	look_at(mouseposition)
+	if Input.is_action_just_pressed("Shoot") and not is_shooting:
+		shoot(mouseposition)
+	
+	if is_shooting:
+		bullet.global_position += bullet_direction * bullet_velocity * delta
+	
+	if bullet.global_position.length() > 1200:
+			_reset_bullet()
+
+func shoot(target_pos: Vector2) -> void:
+	bullet.global_position = $Gun.global_position
+	bullet.show()
+
+	bullet_direction = (target_pos - bullet.global_position).normalized()
+	is_shooting = true
+	
+	
+
+func _reset_bullet() -> void:
+	bullet.global_position = Vector2.ZERO
+	bullet.hide()
+	is_shooting = false
+	
